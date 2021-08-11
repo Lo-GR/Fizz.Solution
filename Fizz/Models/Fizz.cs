c240d0255bf6c39a5cffc4ac5c3113df16570963
Logan Roth <76756897+Lo-GR@users.noreply.github.com>
2021-08-11 12:42:28 -0700

    create Fizz project with complete v1 code

---
+++ Fizz/Models/Fizz.cs
@@ -0,0 +1,41 @@
+using System;
+
+//If number is divisble by 3 print Fizz ,
+//if number is divisible by 5 print buzz.
+//int, non-negative int. 
+namespace Fizz
+{
+    class FizzBuzz
+    {
+        public string[] GenerateFizzBuzz(int n)
+        {
+            string[] fizzBuzzArray = new string[n];
+            // for loop with i correlating with each integer between 0 and n;
+            for (int i = 0; i < n; i++)
+            {
+                int number = i + 1;
+                if(number % 5 == 0 && number % 3 == 0)
+                {
+                    fizzBuzzArray[i] = "FizzBuzz";
+                } 
+                else if (number % 3 == 0)
+                {
+                    fizzBuzzArray[i] = "Fizz";
+                }
+                else if (number % 5 == 0)
+                {
+                    fizzBuzzArray[i] = "Buzz";
+                }
+                else
+                {
+                    string parsedNumber = number.ToString();
+                    fizzBuzzArray[i] = parsedNumber;
+                }
+                //check for integers that are divisible by 3
+                // check for integers that are divisible by 5
+                // if integers are divisible by 5 AND 3, print "FizzBuzz"
+            }
+            return fizzBuzzArray;
+        }
+    }
+}
---
+++ Fizz/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//If number is divisble by 3 print Fizz ,
+//if number is divisible by 5 print buzz.
+//int, non-negative int. 
+// make application distributed individually ( apart from console application)
+// unit testing, for 3 and 5 case
+// Any additional unit testing needed
+// Create ability to redefine pairs of numbers, including unit testing
+
+
+namespace Fizz
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FizzBuzz fizzBuzzInstance = new FizzBuzz();
+            int testNumber = 100;
+            string[] fizzBuzzArray = fizzBuzzInstance.GenerateFizzBuzz(testNumber);
+            for (int i = 0; i < testNumber; i ++)
+            {
+                Console.WriteLine(fizzBuzzArray[i]);
+            }
+        }
+    }
+}
